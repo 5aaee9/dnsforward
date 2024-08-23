ee4862dfe6fa2612d68f20259b4a0a166e3efb6b
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2024-08-23 18:07:13 +0800

    feat: add main code

---
+++ README.md
@@ -0,0 +1,3 @@
+# DNSProxy
+
+Accept client query and add edns client subnet forward to upstream
---
+++ main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/miekg/dns"
+	"log"
+	"net"
+	"net/netip"
+)
+
+func appendEdns0Subnet(msg *dns.Msg, addr net.IP) {
+	newOpt := true
+	var o *dns.OPT
+	for _, v := range msg.Extra {
+		if v.Header().Rrtype == dns.TypeOPT {
+			o = v.(*dns.OPT)
+			newOpt = false
+			break
+		}
+	}
+	if o == nil {
+		o = new(dns.OPT)
+		o.Hdr.Name = "."
+		o.Hdr.Rrtype = dns.TypeOPT
+	}
+	e := &dns.EDNS0_SUBNET{
+		Code:        dns.EDNS0SUBNET,
+		SourceScope: 0,
+		Address:     addr,
+	}
+	if addr.To4() == nil {
+		e.Family = 2 // IP6
+		e.SourceNetmask = net.IPv6len * 8
+	} else {
+		e.Family = 1 // IP4
+		e.SourceNetmask = net.IPv4len * 8
+	}
+	o.Option = append(o.Option, e)
+	if newOpt {
+		msg.Extra = append(msg.Extra, o)
+	}
+}
+
+func main() {
+	listen := flag.String("listen", "0.0.0.0:53", "DNS server listen address")
+	upstream := flag.String("upstream", "1.1.1.1:53", "Upstream dns server")
+	flag.Parse()
+
+	client := &dns.Client{}
+
+	handler := dns.HandlerFunc(func(w dns.ResponseWriter, r *dns.Msg) {
+		remoteAddrPort := netip.MustParseAddrPort(w.RemoteAddr().String())
+		forwardMessage := r.Copy()
+		appendEdns0Subnet(forwardMessage, net.UDPAddrFromAddrPort(remoteAddrPort).IP)
+
+		res, _, err := client.Exchange(forwardMessage, *upstream)
+		if err != nil {
+			return
+		}
+
+		_ = w.WriteMsg(res)
+	})
+
+	udpServer := &dns.Server{
+		Addr:    *listen,
+		Net:     "udp",
+		Handler: handler,
+	}
+
+	tcpServer := &dns.Server{
+		Addr:    *listen,
+		Net:     "tcp",
+		Handler: handler,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		err := udpServer.ListenAndServe()
+		if err != nil {
+			cancel()
+			log.Fatal(err)
+		}
+	}()
+
+	go func() {
+		err := tcpServer.ListenAndServe()
+		if err != nil {
+			cancel()
+			log.Fatal(err)
+		}
+	}()
+
+	<-ctx.Done()
+}
